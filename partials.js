4de4c9b2fa6e839f7cf5f64121d7f8d9c2a83210
jingfei <jingfei955047@gmail.com>
2017-02-24 17:12:39 +0800

    Add research page.

--- partials.js
+++ partials.js
@@ -4,6 +4,33 @@ module.exports= function (hbs) {
   hbs.registerPartial('head', getPartials('head'));
   hbs.registerPartial('header', getPartials('header'));
   hbs.registerPartial('footer', getPartials('footer'));
+
+  hbs.registerHelper('ifCond', function (v1, operator, v2, options) {
+    switch (operator) {
+      case '==':
+        return (v1 == v2) ? options.fn(this) : options.inverse(this);
+      case '===':
+        return (v1 === v2) ? options.fn(this) : options.inverse(this);
+      case '!=':
+        return (v1 != v2) ? options.fn(this) : options.inverse(this);
+      case '!==':
+        return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+      case '<':
+        return (v1 < v2) ? options.fn(this) : options.inverse(this);
+      case '<=':
+        return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+      case '>':
+        return (v1 > v2) ? options.fn(this) : options.inverse(this);
+      case '>=':
+        return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+      case '&&':
+        return (v1 && v2) ? options.fn(this) : options.inverse(this);
+      case '||':
+        return (v1 || v2) ? options.fn(this) : options.inverse(this);
+      default:
+        return options.inverse(this);
+    }
+  });
 };
 
 function getPartials(filename) {
--- route.js
+++ route.js
@@ -1,16 +1,30 @@
 var path = require('path');
 var url = require('url');
 var news = require('./data/news.json');
+var projects = require('./data/projects.json');
 
 route = [
   {
     data: {
       "title": "IMS Lab",
-      "news": news
+      "menu_home": true,
+      "news": news,
+      "baseUrl": "./"
     },
     "partials": './partials.js',
     "layout":  "./view/index.hbs",
     "filename": "./docs/index.html"
+  },
+  {
+    data: {
+      "title": "研究 - IMS Lab",
+      "menu_research": true,
+      "projects": projects,
+      "baseUrl": "../"
+    },
+    "partials": './partials.js',
+    "layout":  "./view/research.hbs",
+    "filename": "./docs/research/index.html"
   }
 ];
 
